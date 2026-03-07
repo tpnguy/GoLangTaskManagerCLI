4885b40d50de313016938422637b5145843ea9ab
tpnguy <clubjuya2@gmail.com>
2026-03-06 22:57:14 -0700

    Git init Go
    This is the initial git commit of the TaskManager CLI app that I'm doing
    to learn GO. I've never touched it until now.

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+import "os"
+
+type Task struct {
+	Title string
+	Done bool
+}
+
+
+func main(){
+	task1 := Task{
+		Title: "This is task 1",
+		Done:  false,
+	}
+	task2 := Task{
+		Title: "This is task 2",
+		Done:  true,
+	}
+	task3 := Task{
+		Title: "This is task 3",
+		Done:  false,
+	}
+
+	tasks := []Task{task1, task2, task3}
+
+
+	if len(os.Args) <= 1 {
+		fmt.Println("Usage: main.go list | main.go add")
+		return
+	} 	
+
+	command := os.Args[1]
+
+	switch command {
+	case "list":
+		for i, v := range tasks {
+			fmt.Println(i, v.Title, v.Done)
+		}
+	case "add":
+		fmt.Println("Nothing for now.")
+	default:
+		fmt.Println("None of the options matched.")
+	}
+}
