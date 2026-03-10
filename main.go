7c105b612128bd16156cdc95c70d529d073129b1
tpnguy <clubjuya2@gmail.com>
2026-03-09 23:19:37 -0700

    Fixed some bugs with DELETE
    There was some incorrect logic that happened. Fixed it so that it
    doesn't rely on the index of the slice, but the index that we chose.
    Also made the counter for ID a bit more safe, as before there was a
    possibility of duplicate IDs.

--- main.go
+++ main.go
@@ -20,7 +20,18 @@ type Task struct {
 
 type App struct {
 	Tasks []Task
+	NextID int
 	Mu sync.Mutex
+}
+
+func nextTaskID(tasks []Task) int {
+	maxID := 0
+	for _, t := range tasks {
+		if t.ID > maxID {
+			maxID = t.ID
+		}
+	}
+	return maxID + 1
 }
 
 
@@ -87,9 +98,13 @@ func (a *App) postTasks(w http.ResponseWriter, r *http.Request){
 	a.Mu.Lock()
 	defer a.Mu.Unlock()
 
-	task.ID = len(a.Tasks) + 1
+	task.ID = a.NextID
+	a.NextID++
 	a.Tasks = append(a.Tasks, task)
-	saveTasks(a.Tasks)
+	if err := saveTasks(a.Tasks); err != nil {
+		http.Error(w, "unable to save task", http.StatusInternalServerError)
+		return
+	}
 
 	enc := json.NewEncoder(w)
 
@@ -106,21 +121,28 @@ func (a *App) deleteTask(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 
 	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
-	index, err := strconv.Atoi(path)
+	id, err := strconv.Atoi(path)
 	if err != nil {
 		http.Error(w, "invalid task id", http.StatusBadRequest)
-		return
-	}
-
-	if index < 0 || index >= len(a.Tasks){
-		http.Error(w, "task index out of range", http.StatusNotFound)
 		return
 	}
 
 	a.Mu.Lock()
 	defer a.Mu.Unlock()
 
-	a.Tasks = append(a.Tasks[:index], a.Tasks[index+1:]...)
+	foundIndex := -1
+	for i := range a.Tasks {
+		if a.Tasks[i].ID == id {
+			foundIndex = i
+			break
+		}
+	}
+	if foundIndex == -1 {
+		http.Error(w, "task not found", http.StatusNotFound)
+		return
+	}
+
+	a.Tasks = append(a.Tasks[:foundIndex], a.Tasks[foundIndex+1:]...)
 	if err := saveTasks(a.Tasks); err != nil {
 		http.Error(w, "failed to save tasks", http.StatusInternalServerError)
 		return
@@ -128,7 +150,7 @@ func (a *App) deleteTask(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]any{
 		"message":"deleted",
-		"index": index,
+		"id": id,
 	})
 }
 
@@ -143,11 +165,23 @@ func (a *App) tasksHandler(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func (a *App) tasksByIDHandler(w http.ResponseWriter, r *http.Request){
+	switch r.Method{
+	case http.MethodDelete:
+		a.deleteTask(w, r)
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
+	tasks := loadTasks()
 	app := &App {
-		Tasks: loadTasks(),
+		Tasks: tasks,
+		NextID: nextTaskID(tasks),
 	}
 	http.HandleFunc("/tasks", app.tasksHandler)
+	http.HandleFunc("/tasks/", app.tasksByIDHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
